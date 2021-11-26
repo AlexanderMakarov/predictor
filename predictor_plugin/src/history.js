e9f3f7efa28865c0083c875d02a58e4ba6d042c2
4ellendger <4ellendger@gmail.com>
2021-11-27 01:41:07 +0300

    Fixes and conflicts resolving.

--- predictor_plugin/src/history.js
+++ predictor_plugin/src/history.js
@@ -1,5 +1,4 @@
 // Handles history. Saves in separate sheet, extracts data to predict.
-const History = new function() {
-    const sheetName = 'PredictorHistory';
-    
+const History = {
+    sheetName: 'PredictorHistoryTest'
 }
