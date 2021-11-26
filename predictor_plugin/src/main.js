774a2fa4de6b47f12f342e28e957f0654afd4c3e
Alexandr Shurenkov <shurenkov@alkor.co.ru>
2021-11-26 16:48:47 +0300

    predictor: add ability to read data from different histories.

--- predictor_plugin/src/main.js
+++ predictor_plugin/src/main.js
@@ -17,12 +17,81 @@ function addMenu() {
       .createMenu('Predictor')
       .addItem("Show Predictor UI", "addSiderBar")
       .addSeparator()
-      .addItem("Predict Today", "function2")
+      .addItem("Predict Today", "sendHistoryAndRedraw")
       .addToUi();
 }
   
-function myFunction() {
+/*function myFunction() {
   Logger.log(SpreadsheetApp.getActiveSpreadsheet().getSheets().map(sheet => sheet.getName()))
   // SpreadsheetApp.getActiveSheet()
+}*/
+
+
+function getHistory() {
+  let isEts = false;
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  let sheet = isEts ? ss.getSheetByName("etsHistory") : ss.getSheetByName(History.sheetName);
+  let numRows = sheet.getDataRange().getNumRows();
+  let numColumns = sheet.getDataRange().getNumColumns();
+  Logger.log("numRows: " + numRows)
+  Logger.log("numColumns: " + numColumns)
+  Logger.log("==========")
+  let historyDtosForMl = [];
+
+  values = getPairColumnsValues(sheet, 1, numRows, numColumns);
+  historyDtosForMl = historyDtosForMl.concat(getMlDtos(values, getHeaders(values)));
+  Logger.log(getPairColumnsValues(sheet, 1, numRows, numColumns));
+  Logger.log(historyDtosForMl);
+  Logger.log("==========")
 }
-  
+
+function getPairColumnsValues(sheet, columnIndex, numRows, columnOffset) {
+  return sheet.getRange(1, columnIndex, numRows, columnOffset).getValues().filter(item => isItemValid(item));
+}
+
+function getPairColumnsValuesRaw(sheet, columnIndex, numRows, columnOffset) {
+  return sheet.getRange(1, columnIndex, numRows, columnOffset).getValues();
+}
+
+function isItemValid(item) {
+  if (item == null || item == undefined) {
+    return false;
+  }
+  for (let i = 0; i < item.length; i++) {
+     if (item[i] == null || item[i] == undefined || item[i].length == 0) {
+       return false;
+     }
+  }
+  return true;
+}
+
+function getMlDtos(values, headers) {
+  let result = [];
+  for (let i=1; i < values.length; i++) {
+    result.push(getMlDto(values[i], headers))
+  }
+  return result;
+}
+
+function getMlDto(item, headers) {
+  let result = {};
+  for (let i = 0; i < headers.length; i++) {
+    result[headers[i]] = item[i];
+  }
+  return result;
+}
+
+function getHeaders(values) {
+  if (values == null || values == undefined || values.length == 0) {
+    return ["token", "y", "date"];
+  }
+  Logger.log(values[0])
+  isNumberExist = false;
+  for (let i = 0; i < values[0].length; i++) {
+      if (!isNaN(values[0][i])) {
+        return ["token", "y", "date"]
+      }
+  }
+  return values[0];
+}
+
